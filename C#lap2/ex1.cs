347f4b6c18c318a25cd5daa67a032122b05304e2
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2020-05-13 17:51:26 +0700

    lp2

---
+++ C#lap2/ex1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex2_1
+{
+    class ex1
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Random rand = new Random((int)now.Millisecond);
+            int[] Arr = new int[10];
+            for(int x = 0; x < Arr.Length; ++x)
+            {
+                Arr[x] = rand.Next() % 100;
+            }
+            int Total = 0;
+            Console.Write("Arrray value are\t");
+            foreach (int val in Arr)
+            {
+                Total += val;
+                Console.Write(val + ", ");
+            }
+            Console.WriteLine("\nAnd the average is {0,0:f2}",
+                (double)Total/(double)Arr.Length);
+            Console.ReadLine();
+        }
+    }
+}
