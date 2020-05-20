336271e325367459ac698bc445721a2a828584c2
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2020-05-21 05:34:40 +0700

    Create Atom.cs

---
+++ C#lap3/Atom.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Atom
+{
+    class Atom
+    {
+        protected int atomicNumber=1;
+        protected string symbol;
+        protected string fullName;
+        protected double atomicWeight;
+        
+        public int InfautomicNumber
+        {
+            get { return atomicNumber; }
+            set { atomicNumber = value; }
+        }
+
+        public string Infsymbol
+        {
+            get { return symbol; }
+            set { symbol = value; }
+        }
+
+        public string inFfullname
+        {
+            get { return fullName; }
+            set { fullName = value; }
+        }
+
+        public double infatoWeight
+        {
+            get { return atomicWeight; }
+            set { atomicWeight = value; }
+        }
+        /*public Atom(int number,string name,string fullnames,double weight)
+        {
+            this.atomicNumber = number;
+            this.symbol = name;
+            this.fullName = fullnames;
+            this.atomicWeight = weight;
+        }*/
+        public bool Accepts()
+        {
+            
+
+            bool a = Convert.ToBoolean(atomicNumber);
+            bool b = Convert.ToBoolean(symbol);
+            bool c = Convert.ToBoolean(fullName);
+            bool d = Convert.ToBoolean(atomicWeight);
+
+            return true;
+        }
+        public void display()
+        {
+            Console.WriteLine(atomicNumber +"," + symbol + "," + fullName + "," + atomicWeight);
+        }
+        static void Main()
+        {
+            Atom a1 = new Atom();
+            a1.Accepts();
+            a1.display();
+        }
+
+    }
+}
