a7963ad5bd1259b3044f68dba2a94843fad9aee8
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2020-05-21 02:02:07 +0700

    C#

---
+++ hello word/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace epkieu
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int myInt = 2924802;
+            long mylong = myInt;
+            Console.WriteLine(myInt + "->" + mylong);
+
+            double myDouble = 1234.5;
+            int my_int;
+            my_int = (int)myDouble;
+            Console.WriteLine(myDouble + "-->" + my_int);
+
+            myInt = Convert.ToInt32(myDouble);
+            Console.WriteLine(myDouble +"-->"+ myInt);
+           
+           
+        }
+    }
+}
