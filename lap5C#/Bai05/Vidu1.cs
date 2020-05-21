3206e438a9ee3f10d2ad9ea33b55b2e94c03f70f
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2020-05-21 17:29:21 +0700

    C#lap5

---
+++ lap5C#/Bai05/Vidu1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bai05
+{
+    class Vidu1
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[5];
+            try
+            {
+                for (int i = 0; i <= 5; ++i)
+                {
+                    Console.WriteLine("a{0} = ", i + 1);
+                    a[i] = Convert.ToByte(Console.ReadLine());
+                }
+            }catch(FormatException ex)
+            {
+                Console.WriteLine("Khong duoc dang ki tu cho mang so");
+            }catch(OverflowException ex)
+            {
+                Console.WriteLine("khong duoc nhap gia tri nam ngoai mien  0 -255");
+            }catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("loi vuot qua pham vi cuar mang");
+            }
+
+            for(int i=0; i < 5; i++)
+            {
+                Console.Write("{0}", a[i]);
+            }
+        }
+    }
+}
---
+++ lap5C#/ExHandling2/ExHandling2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class InvalidInput : ApplicationException
+{
+    public InvalidInput():base("enter a number greater than Zero") { }
+}
+class TestExcept
+{
+    public static void Main()
+    {
+        int intCnt;
+        int intNumber = 0;
+        Console.WriteLine("enter a number:");
+        try
+        {
+            intNumber = Convert.ToInt32(Console.ReadLine());
+            if(intNumber <= 0)
+            {
+                throw new InvalidInput();
+            }
+        }catch(InvalidInput objInvalidInput)
+        {
+            Console.WriteLine(objInvalidInput.Message);
+        }catch(System.FormatException objFormatException)
+        {
+            Console.WriteLine(objFormatException.Message);
+        }
+        finally
+        {
+            if (intNumber > 0)
+            {
+                for(intCnt = 1; intCnt <=10;intCnt++)
+                {
+                    Console.WriteLine(intCnt * intNumber);
+                }
+            }
+        }
+        Console.ReadLine();
+    }
+}
